f0b28c8e95dffb8c5fb09905f6e0e2590e166b44
Lala Sabathil <aiko@aitsys.dev>
2022-06-27 22:36:20 +0200

    Update PhabricatorPhurlConfigOptions.php

--- src/applications/config/option/PhabricatorPhurlConfigOptions.php
+++ src/applications/config/option/PhabricatorPhurlConfigOptions.php
@@ -28,15 +28,20 @@ Set the domains that Phurl will use to share shortened URLs.
 
 You may have to configure the domains in your DNS server to point to **%s**.
 EOTEXT, $domain));
-    $phurlDomainsExample = array('https://s.phurl.io', 'https://s.phurl.dev');
+
+    $phurlDomainsExample = array('https://s.phurl.dev');
     $phurlDomainsExample = id(new PhutilJSON())->encodeAsList($phurlDomainsExample);
+
+    $phurlMultiDomainsExample = array('https://s.phurl.io', 'https://s.phurl.dev');
+    $phurlMultiDomainsExample = id(new PhutilJSON())->encodeAsList($phurlDomainsExample);
 
     return array(
       $this->newOption('phurl.short-uri', 'wild', array())
         ->setLocked(false)
         ->setSummary(pht('Domains that Phurl will use to shorten URLs.'))
         ->setDescription($phurlDomainsHelp)
-        ->addExample($phurlDomainsExample, pht('Multi Domain Example')),
+        ->addExample($phurlDomainsExample, pht('Single Domain Example'))
+        ->addExample($phurlMultiDomainsExample, pht('Multi Domain Example')),
     );
   }
 }
