94a6300c0e8de3679fb965588d68eb83d5cb59d3
Lala Sabathil <aiko@aitsys.dev>
2022-06-27 22:17:07 +0200

    Update PhabricatorPhurlConfigOptions.php

--- src/applications/config/option/PhabricatorPhurlConfigOptions.php
+++ src/applications/config/option/PhabricatorPhurlConfigOptions.php
@@ -20,16 +20,16 @@ final class PhabricatorPhurlConfigOptions
   }
 
   public function getOptions() {
+    $phurlDomainsHelp = pht('Set the URI that Phurl will use to share shortened URLs.');
+    $phurlDomainsExample = array('https://s.phurl.io', 'https://s.phurl.dev');
+    $phurlDomainsExample = id(new PhutilJSON())->encodeAsList($phurlDomainsExample);
+
     return array(
-      $this->newOption('phurl.short-uri', 'string', null)
-        ->setLocked(true)
-        ->setSummary(pht('URI that Phurl will use to shorten URLs.'))
-        ->setDescription(
-          pht(
-            'Set the URI that Phurl will use to share shortened URLs.'))
-        ->addExample(
-          'https://some-very-short-domain.museum/',
-          pht('Valid Setting')),
+      $this->newOption('phurl.short-uri', 'wild', array())
+        ->setLocked(false)
+        ->setSummary(pht('Domains that Phurl will use to shorten URLs.'))
+        ->setDescription($phurlDomainsHelp)
+        ->addExample($phurlDomainsExample, pht('Multi Domain Example')),
     );
   }
 }
