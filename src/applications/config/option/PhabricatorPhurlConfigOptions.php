de1730e8f7c527fa859e54b3d31950debfea8309
Lala Sabathil <aiko@aitsys.dev>
2022-06-27 22:34:55 +0200

    Update PhabricatorPhurlConfigOptions.php

--- src/applications/config/option/PhabricatorPhurlConfigOptions.php
+++ src/applications/config/option/PhabricatorPhurlConfigOptions.php
@@ -26,7 +26,7 @@ final class PhabricatorPhurlConfigOptions
     $phurlDomainsHelp = $this->deformat(pht(<<<EOTEXT
 Set the domains that Phurl will use to share shortened URLs.
 
-You may have to configure the domains in your DNS server to point to %s.
+You may have to configure the domains in your DNS server to point to **%s**.
 EOTEXT, $domain));
     $phurlDomainsExample = array('https://s.phurl.io', 'https://s.phurl.dev');
     $phurlDomainsExample = id(new PhutilJSON())->encodeAsList($phurlDomainsExample);
