ebac441c2c98d4c7130246c9bf2c4ebdb2a44c19
Lala Sabathil <aiko@aitsys.dev>
2022-06-29 01:14:47 +0200

    Update PhabricatorUsersProfileViewController.php

--- src/applications/users/controller/PhabricatorUsersProfileViewController.php
+++ src/applications/users/controller/PhabricatorUsersProfileViewController.php
@@ -117,7 +117,7 @@ Description:
     ->withAccountTypes(array('discord'))
     ->execute();
 
-    phlog($externalAccounts[1]);
+    phlog($externalAccounts[1]->getAccountIdentifiers());
 
     if (!$view->hasAnyProperties()) {
       return null;
