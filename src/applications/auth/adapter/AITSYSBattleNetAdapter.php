548908be96e2aa2ce70b923eb7844e9cbe5e287a
Lala Sabathil <aiko@aitsys.dev>
2022-10-23 04:54:36 +0200

    add bnet

---
+++ src/applications/auth/adapter/AITSYSBattleNetAdapter.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Authentication adapter for Discord OAuth2.
+ */
+final class AITSYSBattleNetAdapter extends PhutilOAuthAuthAdapter {
+
+  public function getAdapterType() {
+    return 'bnet';
+  }
+
+  public function getAdapterDomain() {
+    return 'blizzard.com';
+  }
+
+  public function getAccountID() {
+    $user = $this->getOAuthAccountData('id');
+    return $user;
+  }
+
+  public function getAccountEmail() {
+    $user = $this->getOAuthAccountData('email');
+    return $user;
+  }
+
+  public function getAccountName() {
+    $user = "{$this->getOAuthAccountData('username')}#{$this->getOAuthAccountData('discriminator')}";
+    return $user;
+  }
+
+  public function getAccountImageURI() {
+    return null;
+  }
+
+  public function getAccountURI() {
+    return null;
+  }
+
+  public function getAccountRealName() {
+    return $this->getOAuthAccountData('name');
+  }
+
+  protected function getAuthenticateBaseURI() {
+    return ' https://oauth.battle.net/oauth/authorize ';
+  }
+
+  protected function getTokenBaseURI() {
+    return ' https://oauth.battle.net/oauth/token';
+  }
+
+  public function getScope() {
+    $scopes = array(
+      'openid',
+    );
+
+    return implode(' ', $scopes);
+  }
+
+  public function getExtraAuthenticateParameters() {
+    return array(
+      'response_type' => 'code',
+      'prompt' => 'none',
+    );
+  }
+
+  public function getExtraTokenParameters() {
+    return array(
+      'grant_type' => 'authorization_code',
+    );
+  }
+
+  protected function loadOAuthAccountData() {
+    return id(new AITSYSBattleNetFuture())
+      ->setAccessToken($this->getAccessToken())
+      ->setRawBNetQuery('userinfo')
+      ->resolve();
+  }
+
+  public function supportsTokenRefresh() {
+    return true;
+  }
+
+  public function getExtraRefreshParameters() {
+    return array(
+      'grant_type' => 'refresh_token',
+    );
+  }
+
+}
---
+++ src/applications/auth/future/AITSYSBattleNetFuture.php
@@ -0,0 +1,137 @@
+<?php
+
+final class AITSYSBattleNetFuture extends FutureProxy {
+
+  private $future;
+  private $clientID;
+  private $accessToken;
+  private $action;
+  private $params;
+  private $method = 'GET';
+  private $region = 'eu';
+
+  public $version = '1.0.0';
+
+  public function __construct() {
+    parent::__construct(null);
+  }
+
+  public function setAccessToken($token) {
+    $this->accessToken = $token;
+    return $this;
+  }
+
+  public function setClientID($client_id) {
+    $this->clientID = $client_id;
+    return $this;
+  }
+
+  public function setRegion($region) {
+    $this->region = $region;
+    return $this;
+  }
+
+  public function setRawBNetQuery($action, array $params = array()) {
+    $this->action = $action;
+    $this->params = $params;
+    return $this;
+  }
+
+  public function setMethod($method) {
+    $this->method = $method;
+    return $this;
+  }
+
+  protected function getProxiedFuture() {
+    if (!$this->future) {
+      $params = $this->params;
+
+      if (!$this->action) {
+        throw new Exception(pht('You must %s!', 'setRawBNetQuery()'));
+      }
+
+      if (!$this->accessToken) {
+        throw new Exception(pht('You must %s!', 'setAccessToken()'));
+      }
+
+      if (!$this->region) {
+        throw new Exception(pht('You must %s!', 'setRegion()'));
+      }
+
+      $uri = new PhutilURI('https://oauth.battle.net/');
+      $uri->setPath('oauth/'.$this->action);
+
+      $future = new HTTPSFuture($uri);
+      $future->setData($this->params);
+      $future->addHeader('Authorization', 'Bearer '.$this->accessToken);
+      //$future->addHeader(':region', $this->region);
+      $future->addHeader('User-Agent', "Phabricator OAuth (https://github.com/Aiko-IT-Systems/Phabricator, {$this->version})");
+      $future->setMethod($this->method);
+
+      $this->future = $future;
+    }
+
+    return $this->future;
+  }
+
+  protected function didReceiveResult($result) {
+    list($status, $body, $headers) = $result;
+
+    if ($status->isError()) {
+      if ($this->isRateLimitResponse($status, $headers)) {
+        // Do nothing, this is a rate limit.
+      } else if ($this->isNotModifiedResponse($status)) {
+        // Do nothing, this is a "Not Modified" response.
+      } else {
+        // This is an error condition we do not expect.
+        throw $status;
+      }
+    }
+
+    $data = null;
+    try {
+      if (strlen($body)) {
+        $data = phutil_json_decode($body);
+      } else {
+        // This can happen for 304 responses.
+        $data = array();
+      }
+    } catch (PhutilJSONParserException $ex) {
+      throw new PhutilProxyException(
+        pht('Expected JSON response from Battle.net.'),
+        $ex);
+    }
+
+    if (idx($data, 'errors')) {
+      $errors = print_r($data['errors'], true);
+      throw new Exception(
+        pht(
+          'Received errors from Battle.net: %s',
+          $errors));
+    }
+
+    return $data;
+  }
+
+  private function isNotModifiedResponse($status) {
+    return ($status->getStatusCode() == 304);
+  }
+
+  private function isRateLimitResponse($status, array $headers) {
+    if ($status->getStatusCode() != 403) {
+      return false;
+    }
+
+    foreach ($headers as $header) {
+      list($key, $value) = $header;
+      if (phutil_utf8_strtolower($key) === 'x-ratelimit-remaining') {
+        if (!(int)$value) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+}
