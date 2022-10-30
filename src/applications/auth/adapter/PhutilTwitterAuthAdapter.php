fdd3a7aa0ac5adb42b5b37ba1e93a3ed488c38af
Lala Sabathil <aiko@aitsys.dev>
2022-10-30 14:32:03 +0100

    update twitter auth

--- src/applications/auth/adapter/PhutilTwitterAuthAdapter.php
+++ src/applications/auth/adapter/PhutilTwitterAuthAdapter.php
@@ -1,37 +1,12 @@
 <?php
 
 /**
- * Authentication adapter for Twitter OAuth1.
+ * Authentication adapter for Twitter OAuth2.
  */
-final class PhutilTwitterAuthAdapter extends PhutilOAuth1AuthAdapter {
+final class PhutilTwitterAuthAdapter extends PhutilOAuthAuthAdapter {
 
   private $userInfo;
-
-  public function getAccountID() {
-    return idx($this->getHandshakeData(), 'user_id');
-  }
-
-  public function getAccountName() {
-    return idx($this->getHandshakeData(), 'screen_name');
-  }
-
-  public function getAccountURI() {
-    $name = $this->getAccountName();
-    if (strlen($name)) {
-      return 'https://twitter.com/'.$name;
-    }
-    return null;
-  }
-
-  public function getAccountImageURI() {
-    $info = $this->getUserInfo();
-    return idx($info, 'profile_image_url');
-  }
-
-  public function getAccountRealName() {
-    $info = $this->getUserInfo();
-    return idx($info, 'name');
-  }
+  private $extendedUserInfo;
 
   public function getAdapterType() {
     return 'twitter';
@@ -41,35 +16,99 @@ final class PhutilTwitterAuthAdapter extends PhutilOAuth1AuthAdapter {
     return 'twitter.com';
   }
 
-  protected function getRequestTokenURI() {
-    return 'https://api.twitter.com/oauth/request_token';
+  public function getAccountID() {
+    $user = $this->getOAuthAccountData('id');
+    return $user;
   }
 
-  protected function getAuthorizeTokenURI() {
-    return 'https://api.twitter.com/oauth/authorize';
-  }
-
-  protected function getValidateTokenURI() {
-    return 'https://api.twitter.com/oauth/access_token';
-  }
-
-  private function getUserInfo() {
-    if ($this->userInfo === null) {
+  public function getAccountEmail() {
+    /*if ($this->extendedUserInfo === null) {
       $params = array(
-        'user_id' => $this->getAccountID(),
+        'skip_status' => true,
+        'include_entities' => false,
+        'include_email' => true,
       );
-
       $uri = new PhutilURI(
-        'https://api.twitter.com/1.1/users/show.json',
+        'https://api.twitter.com/1.1/account/verify_credentials.json',
         $params);
 
       $data = $this->newOAuth1Future($uri)
         ->setMethod('GET')
         ->resolveJSON();
 
-      $this->userInfo = $data;
+      $this->extendedUserInfo = $data;
     }
-    return $this->userInfo;
+    return $this->extendedUserInfo;*/
   }
 
+  public function getAccountName() {
+    $user = $this->getOAuthAccountData('username');
+    return $user;
+  }
+
+  public function getAccountImageURI() {
+    $user = $this->getOAuthAccountData('profile_image_url');
+    return $user;
+  }
+
+  public function getAccountURI() {
+    $user = $this->getOAuthAccountData('url');
+    return $user;
+  }
+
+  public function getAccountRealName() {
+    $user = $this->getOAuthAccountData('name');
+    return $user;
+  }
+
+  protected function getTokenBaseURI() {
+    return 'https://twitter.com/i/oauth2/authorize';
+  }
+
+  protected function getRequestTokenURI() {
+    return 'https://api.twitter.com/2/oauth2/token';
+  }
+
+  public function getScope() {
+    $scopes = array(
+      'offline.access',
+      'follows.read',
+      'users.read',
+      'tweet.read',
+    );
+
+    return implode(' ', $scopes);
+  }
+
+  public function getExtraAuthenticateParameters() {
+    return array(
+      'response_type' => 'code',
+      'code_challenge' => 'challenge',
+      'code_challenge_method' => 'plain',
+    );
+  }
+
+  public function getExtraTokenParameters() {
+    return array(
+      'grant_type' => 'authorization_code',
+      'code_verifies' => 'challenge',
+    );
+  }
+
+  protected function loadOAuthAccountData() {
+    return id(new AITSYSTwitterFuture())
+      ->setAccessToken($this->getAccessToken())
+      ->setRawTwitterQuery('users/me')
+      ->resolve();
+  }
+
+  public function supportsTokenRefresh() {
+    return true;
+  }
+
+  public function getExtraRefreshParameters() {
+    return array(
+      'grant_type' => 'refresh_token',
+    );
+  }
 }
