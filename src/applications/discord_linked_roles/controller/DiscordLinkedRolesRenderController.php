620a63237b0aa230c2f91b447ed2b446789b66db
Lala Sabathil <aiko@aitsys.dev>
2022-12-23 20:22:42 +0100

    Update DiscordLinkedRolesRenderController.php

--- src/applications/discord_linked_roles/controller/DiscordLinkedRolesRenderController.php
+++ src/applications/discord_linked_roles/controller/DiscordLinkedRolesRenderController.php
@@ -67,7 +67,7 @@ final class DiscordLinkedRolesRenderController extends PhabricatorController {
 
     $data = $this->getData($user);
     $paste = id(new PhabricatorPaste())
-      ->attachContent($data)
+      ->attachContent(id(new PhutilJSON())->encodeFormatted($data))
       ->setTitle('user_data.json')
       ->setLanguage('json');
     $lines = phutil_split_lines($paste->getContent());
