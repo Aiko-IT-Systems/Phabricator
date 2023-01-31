98d6390a182857a384347e000ce0530d51590b2c
Lala Sabathil <aiko@aitsys.dev>
2023-02-01 00:03:45 +0100

    REEEEEEEE

--- src/applications/auth/adapter/AITSYSDiscordAdapter.php
+++ src/applications/auth/adapter/AITSYSDiscordAdapter.php
@@ -172,9 +172,9 @@ final class AITSYSDiscordAdapter extends PhutilOAuthAuthAdapter {
       $custom_field_map[$custom_field_key] = $custom_field_value;
     }
 
-    $this->isStaff = (bool)custom_field_map[0];
-    $this->isMod = (bool)custom_field_map[1];
-    $this->isLeader = (bool)custom_field_map[2];
+    $this->isStaff = (bool)$custom_field_map["std:user:aitsys:is_staff"];
+    $this->isMod = (bool)$custom_field_map["std:user:aitsys:is_moderator"];
+    $this->isLeader = (bool)$custom_field_map["std:user:aitsys:is_leader"];
   }
 
   public function PushAccountMetadata(string $email) {
