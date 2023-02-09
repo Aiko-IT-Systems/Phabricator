3684ae84ff8279ab537cb88ff47965f57badd944
Lala Sabathil <aiko@aitsys.dev>
2023-02-10 00:05:13 +0100

    Update behavior-form-file-validation.js

--- webroot/rsrc/js/core/behavior-form-file-validation.js
+++ webroot/rsrc/js/core/behavior-form-file-validation.js
@@ -5,6 +5,14 @@
 
 JX.behavior('aphront-form-file-validation', function() {
   JX.Stratcom.listen('change', null, function(e) {
-    JX.log(e.getRawEvent().target.files[0].size / 1024 / 1024 + 'MB');
+    JX.log(bytesToSize(e.getRawEvent().target.files[0].size));
   });
+
+  function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    const i = Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), sizes.length - 1);
+    if (i === 0) return `${bytes} ${sizes[i]}`;
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
+  }
 });
