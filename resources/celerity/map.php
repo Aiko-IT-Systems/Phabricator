a371d25006ed7991e3f8aeab08c5d3c9f975ec34
Lala Sabathil <aiko@aitsys.dev>
2023-02-15 07:55:31 +0100

    meow

--- resources/celerity/map.php
+++ resources/celerity/map.php
@@ -533,7 +533,7 @@ return array(
     'rsrc/js/core/behavior-device.js' => 'ac2b1e01',
     'rsrc/js/core/behavior-drag-and-drop-textarea.js' => '7ad020a5',
     'rsrc/js/core/behavior-fancy-datepicker.js' => '36821f8d',
-    'rsrc/js/core/behavior-form-file-validation.js' => 'd61ad967',
+    'rsrc/js/core/behavior-form-file-validation.js' => 'c412d576',
     'rsrc/js/core/behavior-form.js' => '55d7b788',
     'rsrc/js/core/behavior-gesture.js' => 'b58d1a2a',
     'rsrc/js/core/behavior-global-drag-and-drop.js' => '1cab0e9a',
@@ -651,7 +651,7 @@ return array(
     'javelin-behavior-aphront-basic-tokenizer' => '3b4899b0',
     'javelin-behavior-aphront-drag-and-drop-textarea' => '7ad020a5',
     'javelin-behavior-aphront-form-disable-on-submit' => '55d7b788',
-    'javelin-behavior-aphront-form-file-validation' => 'd61ad967',
+    'javelin-behavior-aphront-form-file-validation' => 'c412d576',
     'javelin-behavior-aphront-more' => '506aa3f4',
     'javelin-behavior-audio-source' => '3dc5ad43',
     'javelin-behavior-audit-preview' => 'b7b73831',
@@ -2120,6 +2120,11 @@ return array(
       'javelin-workboard-card',
       'javelin-workboard-header',
     ),
+    'c412d576' => array(
+      'javelin-behavior',
+      'javelin-stratcom',
+      'javelin-dom',
+    ),
     'c687e867' => array(
       'javelin-behavior',
       'javelin-dom',
@@ -2176,11 +2181,6 @@ return array(
     ),
     'd5d673f9' => array(
       'phui-inline-comment-view-css',
-    ),
-    'd61ad967' => array(
-      'javelin-behavior',
-      'javelin-stratcom',
-      'javelin-dom',
     ),
     'd8a86cfb' => array(
       'javelin-behavior',
